1ab439b7a86e1f72de7a756a0537ef597749dbb6
kihope <kihope12@gmail.com>
2018-07-06 00:37:40 +0200

    revengeos bring up
    Change-Id: I5f5f1a686e1cc9169db917abb204ded9efe73298

--- revengeos.mk
+++ revengeos.mk
@@ -1,8 +1,4 @@
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
-
-# Inherit some common AOSP stuff.
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
@@ -12,14 +8,24 @@ DEVICE_PACKAGE_OVERLAYS += device/asus/T00F/overlay
 
 PRODUCT_RUNTIMES := runtime_libart_default
 
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_T00F
+PRODUCT_NAME := revengeos_T00F
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_T00F
 PRODUCT_MANUFACTURER := asus
 PRODUCT_DEVICE := T00F
 
+# Inherit some common AOSP stuff.
+#TARGET_ARCH := x86
+#TARGET_DENSITY := xhdpi
+#TARGET_BOOT_ANIMATION_RES := 720
+#TARGET_INCLUDE_ARCORE := false
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=WW_T00F \
     BUILD_FINGERPRINT=asus/WW_a500cg/ASUS_T00F:5.0/LRX21V/WW_user_3.24.40.87_20151222_34:user/release-keys \
+
     PRIVATE_BUILD_DESC="a500cg-user 5.0 LRX21V WW_user_3.24.40.87_20151222_34 release-keys"
